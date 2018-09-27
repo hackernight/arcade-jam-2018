f6b7280bae8a29d37c66a2394aa91d0a79e0cc39
Evan Cobb <cobb.evan@gmail.com>
2018-09-26 23:57:11 -0500

    tightening up splash screen, tweens via async/await is nice

--- src/states/splashScreen.js
+++ src/states/splashScreen.js
@@ -20,49 +20,60 @@ class SplashScreen extends Phaser.State {
         this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     }
 
-    create() {
+    async create() {
         // Will start asynchronously loading assets
         this.loadResources();
 
+        await this.showStlSplash()
+        this.animateJ2FLogo()
+
+    }
+
+    async showStlSplash(callback) {
         const splash = new CenteredSprite(this.game, 'logo-stl');
         splash.alpha = 0;
-        const tween = this.game.add.tween(splash).to({
-                alpha: 1
-            },
-            1500,
-            Phaser.Easing.Linear.In,
-            true
-        ).yoyo(true, 1000);
+        const millisToLeaveOnScreen = 1500
+        return new Promise((resolve) => {
+            const tween = this.game.add.tween(splash).to({
+                    alpha: 1
+                },
+                150,
+                Phaser.Easing.Linear.In,
+                true
+            ).yoyo(true, millisToLeaveOnScreen);
 
-        tween.onComplete.add(() => {
-            const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo-studio');
-            logo.anchor.set(0.5);
-            logo.scale.set(2);
+            tween.onComplete.add(() => {resolve()});
+        });
+    }
 
-            logo.animations.add('logo');
-            const animation = logo.animations.play('logo', 15, false);
+    animateJ2FLogo() {
+        const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo-studio');
+        logo.anchor.set(0.5);
+        logo.scale.set(2);
 
-            animation.onComplete.add(() => {
-                const style = {
-                    font: '42px Arial',
-                    fill: '#ffffff',
-                    stroke: 0x333333,
-                    strokeThickness: 5,
-                    align: 'center'
-                };
-                const text = this.game.add.text(this.game.world.centerX, 100, 'Jokes Too Far Games', style);
-                text.alpha = 0;
-                text.anchor.set(0.5);
-                const txtTween = this.game.add.tween(text).to({
-                        alpha: 1
-                    },
-                    2000,
-                    Phaser.Easing.Linear.In,
-                    true
-                );
+        logo.animations.add('logo');
+        return logo.animations.play('logo', 30, false);
 
-                txtTween.onComplete.add(this.moveToNextState, this);
-            });
+        animation.onComplete.add(() => {
+            const style = {
+                font: '42px Arial',
+                fill: '#ffffff',
+                stroke: 0x333333,
+                strokeThickness: 5,
+                align: 'center'
+            };
+            const text = this.game.add.text(this.game.world.centerX, 100, 'Jokes Too Far Games', style);
+            text.alpha = 0;
+            text.anchor.set(0.5);
+            const txtTween = this.game.add.tween(text).to({
+                    alpha: 1
+                },
+                150,
+                Phaser.Easing.Linear.In,
+                true
+            );
+
+            txtTween.onComplete.add(this.moveToNextState, this);
         });
     }
 
