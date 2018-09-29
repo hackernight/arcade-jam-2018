5a8fc80803e1edbb94cd5d07bcdb62ad2efdd469
Becky Asbury <rasbury7@gmail.com>
2018-09-29 15:11:50 -0500

    Menu has instructions

--- src/states/menu.js
+++ src/states/menu.js
@@ -10,10 +10,14 @@ class Menu extends Phaser.State {
 
     create() {
         game.stage.backgroundColor = "#213ad1";
-        const text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'MENU', {
+        const text = this.add.text(this.game.width * 0.5, this.game.height * 0.5,
+          "T-Rex 🦖: throw your eggs 🥚 at the UFO 🛸\n\nUFO 🛸: catch 🎐 all the chickens 🐓"
+          , {
             font: '42px Arial',
             fill: '#ffffff',
-            align: 'center'
+            align: 'center',
+            wordWrap: true,
+            wordWrapWidth: this.game.width/1.5
         });
         text.anchor.set(0.5);
 
