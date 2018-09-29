70a098e02516ab5fb165494dbf9fac4d5f8453af
Pandax0 <Paul.g.mathis@gmail.com>
2018-09-29 16:20:29 -0500

    chick now moves

--- src/prefabs/chicken.js
+++ src/prefabs/chicken.js
@@ -1,6 +1,7 @@
 const SPRITE_SIZE=128;
 const MAX_VELOCITY = 250;
 const ACCELERATION = 25;
+var current_speed = -400;
 
 //Documentation for Phaser's (2.6.2) sprites:: phaser.io/docs/2.6.2/Phaser.Sprite.html
 class Chicken extends Phaser.Sprite {
@@ -18,18 +19,31 @@ class Chicken extends Phaser.Sprite {
       game.add.existing(this);
       // Set Anchor to the center of your sprite
       this.anchor.setTo(.5);
-
+      this.body.velocity.x = current_speed
       //this.walkingSound = this.game.add.audio('walking')
       //this.walkingSound.volume = .3
       //this.digging = this.game.add.audio('digging')
-      this.facing = 'right';
+      //this.facing = 'right';
       // Invert scale.x to flip left/right
       this.scale.x *= -1;
       // game.global.input.bindOnDown('one', 'left', this.moveLeft, this)
   }
 
+
   //Code ran on each frame of game
   update() {
+
+  if (this.right < 0 ){
+    this.body.velocity.x = current_speed * -1
+    this.scale.x *= -1;
+
+  }
+  else if(this.left > this.game.width - this.width) {
+    this.body.velocity.x = current_speed
+    this.scale.x *= -1;
+
+  }
+
   }
 
 
